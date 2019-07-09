a705f092b1eb4674b346d1f73979c510948479c9
donmahallem <donmahallem@users.noreply.github.com>
2019-07-09 09:57:53 +0200

    jJ

--- test.py
+++ test.py
@@ -1,77 +1,89 @@
-#import tensorflow as tf
+from os.path import isfile, join
+from os import listdir
 import numpy as np
-from PIL import Image
-import random
-DEFAULT_SHIPS = [5, 4, 4, 3, 3, 3, 2, 2, 2, 2]
+import cv2 as cv
 
 
-def cell_available(field,  y, x):
-    if field[y, x] != 0:
-        return False
-    if x > 0 and field[y, x-1] != 0:
-        return False
-    if x < 9 and field[y, x+1] != 0:
-        return False
-    if y > 0 and field[y-1, x] != 0:
-        return False
-    if y < 9 and field[y+1, x] != 0:
-        return False
-    return True
+def show(img, wait=True):
+    cv.imshow("JJ", img)
+    cv.waitKey(0 if wait == True else 1)
 
 
-def location_available(field, ship):
-    for coord in ship:
-        if not cell_available(field, coord[0], coord[1]):
-            return False
-    return True
+def normalize(img):
+    vmax = np.max(img)
+    vmin = np.min(img)
+    return (img-vmin)*1.0/(vmax-vmin)
 
 
-def random_fill_boats(ships):
-    field = np.zeros((10, 10))
-    for ship in ships:
-        direction = np.random.randint(0, 2)
-        max_x = 10-ship
-        max_y = 10-ship
-        if direction == 0:
-            # boat is vertical
-            max_x = 10
-        else:
-            max_y = 10
-        while True:
-            start_x = np.random.randint(0, max_x)
-            start_y = np.random.randint(0, max_y)
-            cells = 0
-            if direction == 0:
-                cells = [[x+start_y, start_x] for x in range(ship)]
-            else:
-                cells = [[start_y, x+start_x] for x in range(ship)]
-            if location_available(field, cells):
-                for cell in cells:
-                    field[cell[0], cell[1]] = 1
-                break
-    return field
+def asdf5(img):
+    ssize = 512
+    # img = cv.resize(img, (ssize, ssize), interpolation=cv.INTER_CUBIC)
+    hlsimg = cv.cvtColor(img, cv.COLOR_BGR2HLS)
+    kernel_size = min(128, round(min(img.shape[0], img.shape[1])/50))
+    # print(kernel_size)
+    hlsimg = cv.blur(hlsimg, (kernel_size, kernel_size))  # .astype(np.float32)
+    # show(hlsimg[:, :, 1])
+    luminance_img = np.zeros((img.shape), dtype=np.uint8)
+    exp_img = normalize((hlsimg[:, :, 1]/255.)**2)*-255+255  # *255  # +255
+    # show(exp_img)
+    exp_img = exp_img.astype(np.uint8)
+    thresh1 = cv.adaptiveThreshold(exp_img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                   cv.THRESH_BINARY, 15, 0)
+    #ret, thresh1 = cv.threshold(exp_img, 150, 255, cv.THRESH_BINARY)
+    # print(np.max(thresh1), thresh1.dtype)
+    # show(exp_img)
+    # print(ret, thresh1.shape)
+    # print(np.min(hlsimg[:, :, 1]), np.max(hlsimg[:, :, 1]))
+    # show(thresh1)
+    luminance_img[:, :, 0] = luminance_img[:,
+                                           :, 1] = luminance_img[:, :, 2] = exp_img
+    contours, hierarchy = cv.findContours(
+        thresh1, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    ll = -1
+    lls = -1
+    for idx in range(0, len(contours)):
+        c = contours[idx]
+        #print("CC", len(c))
+        if len(c) > lls:
+            ll = idx
+            lls = len(c)
+    # luminance_img
+    out = np.zeros(img.shape, dtype=np.uint8)
+    # out += 255
+    cv.fillPoly(out, pts=[contours[ll]], color=(255, 255, 255))
+
+    # show(cv.resize(out, (ssize, ssize), interpolation=cv.INTER_CUBIC))
+    # out = cv.blur(out, (kernel_size, kernel_size))
+    out2 = np.zeros(img.shape, dtype=np.uint8)
+    out2[:, :, :] += 255
+    out2 = out2-out
+    out2 = out2+(out/255.*img)
+    # out2 += (img*(out/255.)).astype(np.uint8)  # np.add(img, out)  # +500
+    #show(cv.resize(out2, (ssize, ssize), interpolation=cv.INTER_CUBIC))
+    # print(np.max(out))
+    # out2 += out*8
+    out2 = np.clip(out2, 0, 255)
+    # out2 = (img*(out*1./255.0)).astype(np.uint8)
+
+    # cv.copyTo(img, out2, mask=out)
+    # out2 = img
+    # out2 += out
+    # out2 = np.clip(out2, 0, 255)
+    return out2.astype(np.uint8)
 
 
-COORDINATES = [[x % 10, x//10] for x in range(100)]
-print(random.sample(COORDINATES, 4))
-# If ships are hit or destroyed
+mypath = "./imgs"
+onlyfiles = [f for f in listdir(mypath) if (
+    isfile(join(mypath, f)) and f[-4:] == ".jpg")]
 
+print(onlyfiles)
+for testfile in onlyfiles:
+    img = cv.imread(join(mypath, testfile))
+    ssize = 512
+    im3 = asdf5(img)
+    im4 = cv.resize(im3, (ssize, ssize), interpolation=cv.INTER_CUBIC)
 
-def modify_field(field, ship_tiles=sum(DEFAULT_SHIPS)):
-    shots = random.sample(COORDINATES, np.random.randint(0, 101))
-    hit_count = 0
-    target_map=np.zeros((10,10,3))
-    for shot in range(shots):
-        if field[shot[0],shot[1]] <> 0:
-            hit_count =hit_count+1
-        if hit_count == ship_tiles:
-            return field
-        
-
-test_field = random_fill_boats(DEFAULT_SHIPS)
-print(test_field)
-for s in range(20):
-    test_field[0:10, 1] = 1
-print(test_field)
-img = Image.fromarray(test_field, '1')
-# img.show()
+    # im3 = cv.resize(a, (ssize, ssize), interpolation=cv.INTER_CUBIC)
+    # im3 = cv2.drawContours(im2, contours, -1, (0, 255, 255), 3)
+    show(im4, False)
+    cv.imwrite(join("./outimg", testfile), im3)
